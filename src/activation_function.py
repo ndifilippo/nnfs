78e86e587dfd7fcbb61110e402005cb84eaa1b1a
n-difilippo <nicholas.difilippo7@gmail.com>
2024-06-27 09:08:35 -0500

    adding activation function

---
+++ src/activation_function.py
@@ -0,0 +1,21 @@
+import numpy as np
+# ReLU activation
+
+class Activation_ReLU:
+     # Forward pass
+
+    def forward(self, inputs):
+        # Calculate output values from input
+     
+        self.output = np.maximum(0, inputs)
+
+# Softmax activation
+class Activation_Softmax:
+    # Forward pass
+   def forward(self, inputs):
+       # Get unnormalized probabilities
+      exp_values = np.exp(inputs - np.max(inputs, axis=1,
+                                          keepdims=True))
+      # Normalize them for each sample
+      probabilities = exp_values / np.sum(exp_values, axis=1,keepdims=True)
+      self.output = probabilities
--- src/hidden_layers.py
+++ src/hidden_layers.py
@@ -7,5 +7,7 @@ nnfs.init()
 
 X, y = spiral_data(samples=100, classes=3)
 
+print(X[:, 0])
+
 plt.scatter(X[:, 0], X[:, 1], c=y, cmap='brg')
 plt.show()
