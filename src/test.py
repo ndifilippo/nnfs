b677e96fd1eebbd3c8ef6521028c4bbffc1c83cf
n-difilippo <nicholas.difilippo7@gmail.com>
2024-07-25 10:03:08 -0500

    adding optimization function

--- src/test.py
+++ src/test.py
@@ -30,38 +30,57 @@ activation2 = Activation_Softmax()
 # Create a loss function 
 loss_function = Loss_CategoricalCrossentropy()
 
-#%% 
-# Make a forward pass of our training data through this layer
-dense1.forward(X)
 
 
-#%% 
 
-# Make a forward pass through activation function
-# it takes the output of first dense layer here
-activation1.forward(dense1.output)
+#%%
+# Helper Variables 
+lowest_loss = 9999999
+best_dense1_weights = dense1.weights.copy()
+bets_dense1_biases = dense1.biases.copy()
+best_dense2_weights = dense2.weights.copy()
+bets_dense2_biases = dense2.biases.copy()
 
-#%% 
+for iteration in range(1000):
 
-# Make a forward pass through second Dense layer
-# it takes outputs of activation function of first layer as inputs
-dense2.forward(activation1.output)
+    #Update weights with some small random variable
+    dense1.weights += 0.05 * np.random.randn(2,3)
+    dense1.biases +=  0.05 * np.random.randn(1,3)
+    dense2.weights += 0.05 * np.random.randn(3,3)
+    dense2.biases +=  0.05 * np.random.randn(1,3)
 
+    # Make a forward pass of our training data through this layer
+    dense1.forward(X)
 
-#%% 
+    # Make a forward pass through activation function
+    # it takes the output of first dense layer here
+    activation1.forward(dense1.output)
 
-# Make a forward pass through activation function
-# it takes the output of second dense layer here
-activation2.forward(dense2.output)
+    # Make a forward pass through second Dense layer
+    # it takes outputs of activation function of first layer as inputs
+    dense2.forward(activation1.output)
 
-#%% 
-# Let's see output of the first few samples:
-print(activation2.output[:5])
+    # Make a forward pass through activation function
+    # it takes the output of second dense layer here
+    activation2.forward(dense2.output)
 
-#%% 
-# Perform a forward pass through activation function
-# it takes the output of second dense layer here and returns loss
-loss = loss_function.calculate(activation2.output, y)
-# Print loss value
-print('loss:', loss)
-# %%
+    loss = loss_function.calculate(activation2.output, y)
+
+    # Calculate accuracy from output of activation2 and targets
+    # calculate values along first axis
+    predictions = np.argmax(activation2.output, axis=1)
+    accuracy = np.mean(predictions == y)
+    # If loss is smaller - print and save weights and biases aside
+    if loss < lowest_loss:
+        print('New set of weights found, iteration:', iteration,'loss:', loss, 'acc:', accuracy)
+        best_dense1_weights = dense1.weights.copy()
+        best_dense1_biases = dense1.biases.copy()
+        best_dense2_weights = dense2.weights.copy()
+        best_dense2_biases = dense2.biases.copy()
+        lowest_loss = loss
+    # Revert weights and biases
+    else:
+        dense1.weights = best_dense1_weights.copy()
+        dense1.biases = best_dense1_biases.copy()
+        dense2.weights = best_dense2_weights.copy()
+        dense2.biases = best_dense2_biases.copy()
